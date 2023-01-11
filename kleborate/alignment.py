4dd9280af3309f7eed83fc41a208e3f64754e26f
Ryan Wick <rrwick@gmail.com>
2023-01-11 12:23:34 +1100

    Add code for minimap2 alignment

---
+++ kleborate/alignment.py
@@ -0,0 +1,88 @@
+"""
+Copyright 2023 Kat Holt
+Copyright 2023 Ryan Wick (rrwick@gmail.com)
+https://github.com/katholt/Kleborate/
+
+This file is part of Kleborate. Kleborate is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version. Kleborate is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License along with Kleborate. If
+not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import re
+import subprocess
+import sys
+
+
+class Alignment(object):
+    """
+    Defines a minimap2 alignment. Each object is created from a single line in a minimap2 PAF file.
+
+    It is assumed that minimap2 was run with its -c option so that full alignment is performed. If
+    -c wasn't used, then some pieces will be missing (e.g. CIGAR) and some will be incorrect (e.g.
+    percent_identity).
+    """
+
+    def __init__(self, paf_line):
+        line_parts = paf_line.strip().split('\t')
+        if len(line_parts) < 11:
+            sys.exit('Error: alignment file does not seem to be in PAF format')
+
+        self.query_name = line_parts[0]
+        self.query_length = int(line_parts[1])
+        self.query_start = int(line_parts[2])
+        self.query_end = int(line_parts[3])
+        self.strand = line_parts[4]
+
+        self.target_name = line_parts[5]
+        self.target_length = int(line_parts[6])
+        self.target_start = int(line_parts[7])
+        self.target_end = int(line_parts[8])
+
+        self.matching_bases = int(line_parts[9])
+        self.num_bases = int(line_parts[10])
+        self.percent_identity = 100.0 * self.matching_bases / self.num_bases
+
+        self.query_cov = 100.0 * (self.query_end - self.query_start) / self.query_length
+
+        self.cigar, self.alignment_score = None, None
+        for part in line_parts:
+            if part.startswith('cg:Z:'):
+                self.cigar = part[5:]
+            if part.startswith('AS:i:'):
+                self.alignment_score = int(part[5:])
+
+    def __repr__(self):
+        return self.query_name + ':' + str(self.query_start) + '-' + str(self.query_end) + \
+               '(' + self.strand + '), ' + \
+               self.target_name + ':' + str(self.target_start) + '-' + str(self.target_end) + \
+               ' (' + ('%.3f' % self.percent_identity) + '%)'
+
+
+def align_a_to_b(filename_a, filename_b, preset='asm20'):
+    """
+    Runs minimap2 on two sequence files (FASTA or FASTQ) and returns a list of Alignment objects.
+    The first file (filename_a) is the query and the second (filename_b) is the target.
+    """
+    with open(os.devnull, 'w') as dev_null:
+        out = subprocess.check_output(['minimap2', '--eqx', '-c', '-x', preset,
+                                       str(filename_b), str(filename_a)], stderr=dev_null)
+    return [Alignment(x) for x in out.decode().splitlines()]
+
+
+def get_expanded_cigar(cigar):
+    """
+    Takes in a normal CIGAR string and returns an expanded version.
+    E.g. 5=1D3= -> =====D===
+    """
+    expanded_cigar = []
+    cigar_parts = re.findall(r'\d+[IDX=M]', cigar)
+    for p in cigar_parts:
+        size = int(p[:-1])
+        letter = p[-1]
+        expanded_cigar.append(letter * size)
+    return ''.join(expanded_cigar)
---
+++ test/test_alignment.py
@@ -0,0 +1,41 @@
+"""
+This file contains tests for Kleborate. To run all tests, go the repo's root directory and run:
+  python3 -m pytest
+
+To get code coverage stats:
+  coverage run --source . -m pytest && coverage report -m
+
+Copyright 2023 Kat Holt
+Copyright 2023 Ryan Wick (rrwick@gmail.com)
+https://github.com/katholt/Kleborate/
+
+This file is part of Kleborate. Kleborate is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version. Kleborate is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License along with Kleborate. If
+not, see <https://www.gnu.org/licenses/>.
+"""
+
+import pytest
+
+import kleborate.alignment
+
+
+def test_bad_paf():
+    with pytest.raises(SystemExit) as e:
+        kleborate.alignment.Alignment('not_a_paf_line')
+    assert 'PAF format' in str(e.value)
+
+
+def test_repr():
+    a = kleborate.alignment.Alignment('A\t1000\t50\t150\t+\t'
+                                      'C\t1000\t60\t160\t100\t100\tAS:i:100\tcg:Z:100=')
+    assert str(a) == 'A:50-150(+), C:60-160 (100.000%)'
+
+
+def test_get_expanded_cigar():
+    assert kleborate.alignment.get_expanded_cigar('5=') == '====='
+    assert kleborate.alignment.get_expanded_cigar('3=1I4=2D2=1X4=') == '===I====DD==X===='
+    assert kleborate.alignment.get_expanded_cigar('') == ''
