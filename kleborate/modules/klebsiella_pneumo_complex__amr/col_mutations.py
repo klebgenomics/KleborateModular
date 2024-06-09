6d23d520b02ed35b033aaff6dfe21b65d405a97c
Mary Maranga <38289520+Marysteph@users.noreply.github.com>
2024-06-09 18:33:06 +0100

    added codon checks

--- kleborate/modules/klebsiella_pneumo_complex__amr/col_mutations.py
+++ kleborate/modules/klebsiella_pneumo_complex__amr/col_mutations.py
@@ -19,8 +19,10 @@ from Bio.Align import substitution_matrices
 from ...shared.alignment import align_query_to_ref, is_exact_aa_match, translate_nucl_to_prot, check_for_exact_aa_match, truncation_check
 
 
-def check_for_mgrb_pmrb_gene_truncations(hits_dict, assembly, trunc,min_ident):
+def check_for_mgrb_pmrb_gene_truncations(hits_dict, assembly, trunc, min_ident):
     best_mgrb_cov, best_pmrb_cov = 0.0, 0.0
+    mgrb_hit, pmrb_hit = None, None
+    start_codons = {'TTG', 'CTG', 'ATT', 'ATC', 'ATA', 'ATG', 'GTG'}
 
     alignment_hits = align_query_to_ref(trunc, assembly, None, min_identity=None)
     for hit in alignment_hits:
@@ -29,16 +31,55 @@ def check_for_mgrb_pmrb_gene_truncations(hits_dict, assembly, trunc,min_ident):
         
         if hit.query_name == 'mgrB' and coverage > best_mgrb_cov:
             best_mgrb_cov = coverage
+            mgrb_hit = hit.ref_seq
         elif hit.query_name == 'pmrB' and coverage > best_pmrb_cov:
             best_pmrb_cov = coverage
+            pmrb_hit = hit.ref_seq
             
 
     truncations = []
     if best_mgrb_cov < 90.0:
         truncations.append('MgrB-' + ('%.0f' % best_mgrb_cov) + '%')
+    else:
+        # Check if the first 3 bp of the hit is a known start codon for mgrB
+        if mgrb_hit:
+            hit_start_codon = mgrb_hit[:3].upper()
+            if hit_start_codon not in start_codons:
+                truncations.append('mgrB$')
+
     if best_pmrb_cov < 90.0:
         truncations.append('PmrB-' + ('%.0f' % best_pmrb_cov) + '%')
+    else:
+        # Check if the first 3 bp of the hit is a known start codon for pmrB
+        if pmrb_hit:
+            hit_start_codon = pmrb_hit[:3].upper()
+            if hit_start_codon not in start_codons:
+                truncations.append('pmrB$')
 
     if truncations:
         hits_dict['Col_mutations'] += truncations
 
+
+
+# def check_for_mgrb_pmrb_gene_truncations(hits_dict, assembly, trunc,min_ident):
+#     best_mgrb_cov, best_pmrb_cov = 0.0, 0.0
+
+#     alignment_hits = align_query_to_ref(trunc, assembly, None, min_identity=None)
+#     for hit in alignment_hits:
+#         assert hit.query_name == 'pmrB' or hit.query_name == 'mgrB'
+#         _, coverage, _ = truncation_check(hit)
+        
+#         if hit.query_name == 'mgrB' and coverage > best_mgrb_cov:
+#             best_mgrb_cov = coverage
+#         elif hit.query_name == 'pmrB' and coverage > best_pmrb_cov:
+#             best_pmrb_cov = coverage
+            
+
+#     truncations = []
+#     if best_mgrb_cov < 90.0:
+#         truncations.append('MgrB-' + ('%.0f' % best_mgrb_cov) + '%')
+#     if best_pmrb_cov < 90.0:
+#         truncations.append('PmrB-' + ('%.0f' % best_pmrb_cov) + '%')
+
+#     if truncations:
+#         hits_dict['Col_mutations'] += truncations
