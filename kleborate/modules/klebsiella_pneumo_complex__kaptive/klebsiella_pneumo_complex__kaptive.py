fb63090d67e510caf4ecd5183a53167774673b03
Mary Maranga <38289520+Marysteph@users.noreply.github.com>
2024-06-24 01:07:43 +0100

    updated kaptive module

--- kleborate/modules/klebsiella_pneumo_complex__kaptive/klebsiella_pneumo_complex__kaptive.py
+++ kleborate/modules/klebsiella_pneumo_complex__kaptive/klebsiella_pneumo_complex__kaptive.py
@@ -23,10 +23,10 @@ import sys
 import dna_features_viewer
 from dna_features_viewer import GraphicFeature, GraphicRecord
 
-# Add the kaptive directory to the Python path
+# Add the kaptive path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../kaptive')))
 
-from kaptive.database import Database, get_database
+from kaptive.database import Database, get_database, load_database
 from kaptive.misc import check_python_version, check_programs, get_logo, check_cpus, check_file
 from kaptive.assembly import typing_pipeline
 
@@ -85,7 +85,8 @@ all_headers = [
 def get_results(assembly, minimap2_index, args, previous_results):
     full_headers, _ = get_headers()
 
-    k_db, o_db = Database.from_genbank(get_database('kp_k')), Database.from_genbank(get_database('kp_o'))
+    k_db = load_database('kpsc_k')
+    o_db = load_database('kpsc_o')
 
     assembly_path = Path(assembly)
 
@@ -93,7 +94,7 @@ def get_results(assembly, minimap2_index, args, previous_results):
 
     k_results = typing_pipeline(assembly_path, k_db, threads=args.threads)
     if k_results is not None:
-        k_result_table = k_results.as_table()
+        k_result_table = k_results.format('tsv') 
         for line in k_result_table.split('\n'):
             if line:
                 parts = line.split('\t')
@@ -106,7 +107,7 @@ def get_results(assembly, minimap2_index, args, previous_results):
 
     o_results = typing_pipeline(assembly_path, o_db, threads=args.threads)
     if o_results is not None:
-        o_result_table = o_results.as_table()
+        o_result_table = o_results.format('tsv') 
         for line in o_result_table.split('\n'):
             if line:
                 parts = line.split('\t')
@@ -124,6 +125,49 @@ def get_results(assembly, minimap2_index, args, previous_results):
     results_dict = {k: (v if v else '-') for k, v in results_dict.items()}
 
     return results_dict
+    
+# def get_results(assembly, minimap2_index, args, previous_results):
+#     full_headers, _ = get_headers()
+
+#     k_db, o_db = Database.from_genbank(get_database('kp_k')), Database.from_genbank(get_database('kp_o'))
+
+#     assembly_path = Path(assembly)
+
+#     results_dict = {}
+
+#     k_results = typing_pipeline(assembly_path, k_db, threads=args.threads)
+#     if k_results is not None:
+#         k_result_table = k_results.as_table()
+#         for line in k_result_table.split('\n'):
+#             if line:
+#                 parts = line.split('\t')
+#                 for key, value in zip(all_headers, parts):
+#                     header = 'K_' + key.replace(' ', '_')
+#                     if header in full_headers:
+#                         results_dict[header] = value
+#     else:
+#         print("Warning: No gene alignments sufficient for typing. Skipping k_results processing.")
+
+#     o_results = typing_pipeline(assembly_path, o_db, threads=args.threads)
+#     if o_results is not None:
+#         o_result_table = o_results.as_table()
+#         for line in o_result_table.split('\n'):
+#             if line:
+#                 parts = line.split('\t')
+#                 for key, value in zip(all_headers, parts):
+#                     header = 'O_' + key.replace(' ', '_')
+#                     if header in full_headers:
+#                         results_dict[header] = value
+#     else:
+#         print("Warning: No gene alignments sufficient for typing. Skipping o_results processing.")
+
+#     for h in results_dict.keys():
+#         if h not in full_headers:
+#             sys.exit(f'Error: results contained a value ({h}) that is not covered by the full headers')
+
+#     results_dict = {k: (v if v else '-') for k, v in results_dict.items()}
+
+#     return results_dict
 
 
 
